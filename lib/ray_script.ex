fc540ac07126db28c569f8c522ce3f14a87355ed
Bryan Joseph <bryanjos@gmail.com>
2017-01-28 10:53:22 -0600

    Made functions private

--- lib/ray_script.ex
+++ lib/ray_script.ex
@@ -11,6 +11,11 @@ defmodule RayScript do
     |> Enum.to_list
   end
 
+  defp get_modules(build_path) do
+    Path.join([build_path, "**", "*.beam"])
+    |> Path.wildcard
+  end
+
   def compile_module(beam) do
     beam
     |> get_beam
@@ -21,39 +26,34 @@ defmodule RayScript do
     |> to_output
   end
 
-  def get_modules(build_path) do
-    Path.join([build_path, "**", "*.beam"])
-    |> Path.wildcard
-  end
-
-  def get_beam(beam) when is_binary(beam) do
+  defp get_beam(beam) when is_binary(beam) do
     String.to_char_list(beam)
   end
 
-  def get_beam(beam) when is_atom(beam) do
+  defp get_beam(beam) when is_atom(beam) do
     {_, beam, _} = :code.get_object_code(beam)
     beam
   end
 
-  def to_abstract(beam) do
+  defp to_abstract(beam) do
     {:ok, {_, [abstract_code: {_, abstract_code}]}} = :beam_lib.chunks(beam, [:abstract_code])
     abstract_code
   end
 
-  def to_core(abstract_code) do
+  defp to_core(abstract_code) do
     {:ok, _module, core_code} = :compile.forms(abstract_code, [:to_core])
     core_code
   end
 
-  def to_js_ast(abstract_code) do
+  defp to_js_ast(abstract_code) do
     abstract_code
   end
 
-  def to_js_code(js_ast) do
+  defp to_js_code(js_ast) do
     js_ast
   end
 
-  def to_output(js_code) do
+  defp to_output(js_code) do
     js_code
   end
 end
--- test/support/empty.ex
+++ test/support/empty.ex
@@ -1,3 +1,2 @@
 defmodule Empty do
-
 end
